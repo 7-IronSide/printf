f4bc5ffd48937131abea2d72a6aeb791460ed495
WassCodeur <wachioubouraima56@gmail.com>
2022-07-12 22:29:47 +0000

    review the print_bases file

--- print_bases.c
+++ print_bases.c
@@ -10,6 +10,7 @@
  * number into the correct base and returns it as a string
  * Return: the number of char printed
  */
+
 int print_hex(va_list l, flags_t *f)
 {
 	unsigned int num = va_arg(l, unsigned int);
@@ -17,9 +18,11 @@ int print_hex(va_list l, flags_t *f)
 	int count = 0;
 
 	if (f->hash == 1 && str[0] != '0')
-		count += _puts("0x");
-	count += _puts(str);
-	return (count);
+    {
+        count += _puts("0x");
+        count += _puts(str);
+        return (count);
+    }
 }
 
 /**
