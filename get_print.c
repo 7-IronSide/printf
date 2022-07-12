fb40f35fd977dea2238611d3cd13e5b7ad6fd698
WassCodeur <wachioubouraima56@gmail.com>
2022-07-12 22:40:22 +0000

    review the get_print file

--- get_print.c
+++ get_print.c
@@ -34,6 +34,8 @@ int (*get_print(char s))(va_list, flags_t *)
 
 	for (i = 0; i < flags; i++)
 		if (func_arr[i].c == s)
-			return (func_arr[i].f);
+			{
+			    return (func_arr[i].f);
+			}
 	return (NULL);
 }
